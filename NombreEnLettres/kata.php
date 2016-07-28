17da60f676f665371dbb38db82a33f3c0f1ac2aa
Fabien <fabien.barbaud@timeonegroup.com>
2016-07-28 15:17:46 +0200

    Pilote Alexis Co Pilote Björn

--- NombreEnLettres/kata.php
+++ NombreEnLettres/kata.php
@@ -23,17 +23,31 @@ $chiffres = [
     'vingt',
 ];
 
+$dizaine = [
+    "zero",
+    "dix",
+    "vingt",
+    "trente",
+    "quarante",
+    "cinquante",
+    "soixante",
+    "soixante-dix",
+    "quatre-vingt",
+    "quatre-vingt-dix"
+];
+
 function nombreEnLettres($nombre, $chiffres){
 
     $unit = $nombre % 10;
-    $ten = ($nombre/10) % 10;
-    var_dump($ten);
+    $diz = ($nombre/10) % 10;
+    $cent = ($nombre/100) % 10;
+    var_dump($cent);
 
     if ($nombre >= 0 && $nombre <= 20){
         return $chiffres[$nombre];
     } elseif ($nombre < 30) {
         return "vingt ".nombreEnLettres(substr($nombre,1),$chiffres);
-    } elseif ($nombre < 70) {
+    } elseif ($nombre < 100) {
         return "soixante ".nombreEnLettres(substr($nombre,1),$chiffres);
     }
 
