707eed0954bb06f7d57e1b643b7e3a200b3e66a2
Fabien <fabien.barbaud@timeonegroup.com>
2016-07-28 15:09:24 +0200

    Pilote Raphaël Co Pilote Alexis

--- NombreEnLettres/kata.php
+++ NombreEnLettres/kata.php
@@ -25,12 +25,18 @@ $chiffres = [
 
 function nombreEnLettres($nombre, $chiffres){
 
+    $unit = $nombre % 10;
+    $ten = ($nombre/10) % 10;
+    var_dump($ten);
+
     if ($nombre >= 0 && $nombre <= 20){
         return $chiffres[$nombre];
     } elseif ($nombre < 30) {
-       var_dump(str_split($nombre));
+        return "vingt ".nombreEnLettres(substr($nombre,1),$chiffres);
+    } elseif ($nombre < 70) {
+        return "soixante ".nombreEnLettres(substr($nombre,1),$chiffres);
     }
 
 }
 
-var_dump(nombreEnLettres(22, $chiffres));
+var_dump(nombreEnLettres(63, $chiffres));
