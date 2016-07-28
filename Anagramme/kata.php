78588cd0b99bdd8629ddc59995012a87cf175bcd
Fabien <fabien.barbaud@timeonegroup.com>
2016-07-28 11:59:39 +0200

    Pilote Fabien Copilote Alexisz

--- Anagramme/kata.php
+++ Anagramme/kata.php
@@ -1,7 +1,8 @@
 <?php
 $dictionnaire = file('liste.txt', FILE_IGNORE_NEW_LINES);
 
-function anagramme($mot, $dictionnaire) {
+function anagrammes($mot, $dictionnaire) {
+    $mot_array = str_split($mot);
     $anagramme = array();
 
     foreach ($dictionnaire as $index => $item) {
@@ -9,10 +10,9 @@ function anagramme($mot, $dictionnaire) {
         if (strlen($item) == strlen($mot)){
             $mot_dictionnaire = str_split($item);
 
-            if (isInArray($item,$mot_dictionnaire)) {
+            if (isAnagram($mot_array,$mot_dictionnaire)) {
                 $anagramme[] = $item;
             }
-
         }
     }
     $indexMot = array_search($mot, $anagramme);
@@ -20,27 +20,14 @@ function anagramme($mot, $dictionnaire) {
     return $anagramme;
 }
 
-function isInArray($mot_array, $mot_dictionnaire){
-    $bon = true;
-
-    foreach ($mot_array as $lettre) {
-        if(!in_array($lettre,$mot_dictionnaire)){
-            $bon = false;
-            break;
-        }
-    }
-
-    return $bon;
-}
-
 function isAnagram($tab1, $tab2){
     sort($tab1);
     sort($tab2);
     return ($tab1 == $tab2);
 }
-//var_dump(anagramme("marie", $dictionnaire));
+var_dump(anagrammes("marie", $dictionnaire));
 
 $tab = ["m", "a", "r", "i", "e"];
 $tabDic = ["a", "i", "m", "e", "r"];
 //var_dump(isInArray($tab, $tabDic));
-var_dump(isAnagram($tab, $tabDic));
+//var_dump(isAnagram($tab, $tabDic));
