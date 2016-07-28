455fa50e746abe5d4c47d8955e03fbf9e236c4a5
Fabien <fabien.barbaud@timeonegroup.com>
2016-07-28 10:47:04 +0200

    Pilote fx Copilote Guillaume

--- Anagramme/kata.php
+++ Anagramme/kata.php
@@ -1,2 +1,12 @@
 <?php
 $dictionnaire = file('liste.txt', FILE_IGNORE_NEW_LINES);
+
+function anagramme($mot, $dictionnaire) {
+    $mot_array = str_split($mot);
+
+    foreach ($dictionnaire as $index => $item) {
+
+    }
+}
+
+anagramme("marie", $dictionnaire);
