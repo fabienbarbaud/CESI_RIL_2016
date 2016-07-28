11d79488fcd9f964b1b0bd451a8246b9622b2706
Fabien <fabien.barbaud@timeonegroup.com>
2016-07-28 11:04:52 +0200

    Pilote Axel Copilote Marc

--- Anagramme/kata.php
+++ Anagramme/kata.php
@@ -3,14 +3,15 @@ $dictionnaire = file('liste.txt', FILE_IGNORE_NEW_LINES);
 
 function anagramme($mot, $dictionnaire) {
     $mot_array = str_split($mot);
-
+    $anagramme = array();
     foreach ($dictionnaire as $index => $item) {
 
         if (strlen($item) == strlen($mot)){
             $mot_dictionnaire = str_split($item);
             foreach ($mot_array as $lettre) {
-                if(in_array($lettre,$mot_dictionnaire)){
-
+                if(!in_array($lettre,$mot_dictionnaire)){
+                    $bon = false;
+                    break;
                 }
             }
 
