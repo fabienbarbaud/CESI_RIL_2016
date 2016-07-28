44c248b47637438b93be13087f5bc77ef6505c96
Fabien <fabien.barbaud@timeonegroup.com>
2016-07-28 11:10:41 +0200

    Pilote Marc Copilote Bassam

--- Anagramme/kata.php
+++ Anagramme/kata.php
@@ -4,19 +4,28 @@ $dictionnaire = file('liste.txt', FILE_IGNORE_NEW_LINES);
 function anagramme($mot, $dictionnaire) {
     $mot_array = str_split($mot);
     $anagramme = array();
+
     foreach ($dictionnaire as $index => $item) {
 
         if (strlen($item) == strlen($mot)){
             $mot_dictionnaire = str_split($item);
+            $bon = true;
+
             foreach ($mot_array as $lettre) {
                 if(!in_array($lettre,$mot_dictionnaire)){
                     $bon = false;
                     break;
                 }
+
+                if ($bon == true) {
+                    $anagramme[] = $mot_dictionnaire;
+                }
             }
+
 
         }
     }
+    return $anagramme;
 }
 
-anagramme("marie", $dictionnaire);
+var_dump(anagramme("marie", $dictionnaire));
